8e2809cb27da7c202c8e8af4bb6637af45f650a6
Sameera Madushan Silva <madushansameera499@gmail.com>
2024-08-28 23:18:09 +0530

    add chart

--- controller/script.js
+++ controller/script.js
@@ -74,3 +74,55 @@ $('#order_details_nav').on('click', () => {
     $('#order_nav').removeClass('active-page');
     $('#order_details_nav').addClass('active-page');
 });
+
+
+const ctx2 = document.querySelector('.prog-chart');
+
+new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        datasets: [{
+            label: 'Daily Orders',
+            data: [6, 10, 8, 14, 6, 7],
+            borderColor: '#0891b2',
+            tension: 0.4
+        },
+            {
+                label: 'Aver Orders',
+                data: [8, 6, 7, 6, 11, 8],
+                borderColor: '#ca8a04',
+                tension: 0.4
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        scales: {
+            x: {
+                grid: {
+                    display: false,
+                }
+            },
+            y: {
+                ticks: {
+                    display: false
+                },
+                border: {
+                    display: false,
+                    dash: [5, 5]
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInOutQuad',
+        }
+    }
+});
